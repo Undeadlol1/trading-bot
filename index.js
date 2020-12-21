2050eaaae785e8b65445dfc8c92b87faa54bba1b
undeadlol1 <undeadlol1@mail.ru>
2020-12-22 00:57:22 +0500

    refactor: :recycle:

--- index.js
+++ index.js
@@ -2,49 +2,53 @@ let ticks = 0;
 let totalProfit = 0;
 let amountOfDeals = 0;
 let hasCurrency = false;
+const maximumTicks = 300;
 let priceToBuyAt = undefined;
 let priceToSellAt = undefined;
+const intervalDuration = 1000;
 const profitPercentage = 0.002;
-const intervalDuration = 2000;
 
-const ccxt = require("ccxt");
-const exchange = new ccxt.kraken({ enableRateLimit: true });
+const ccxt = require('ccxt');
+const exchange = new ccxt.binance({ enableRateLimit: false });
 
 setInterval(runStrategy, intervalDuration);
 
 async function runStrategy() {
   ticks++;
 
-  const ticker = await exchange.fetchTicker("ETH/USDT");
+  const ticker = await exchange.fetchTicker('ETH/USDT');
   const currentPrice = ticker && ticker.close;
-  console.log("currentPrice", currentPrice.toFixed(2));
-  console.log("priceToBuyAt: ", priceToBuyAt && priceToBuyAt.toFixed(2));
-  console.log("priceToSellAt: ", priceToBuyAt && priceToSellAt.toFixed(2));
-  console.log("amountOfDeals: ", amountOfDeals);
-  console.log("totalProfit: ", totalProfit);
-  console.log("hasCurrency: ", hasCurrency);
-  console.log("ticks: ", ticks);
+  console.log('currentPrice: ', currentPrice.toFixed(2));
+  console.log('priceToBuyAt: ', priceToBuyAt && priceToBuyAt.toFixed(2));
+  console.log('priceToSellAt: ', priceToBuyAt && priceToSellAt.toFixed(2));
+  console.log('amountOfDeals: ', amountOfDeals);
+  console.log('totalProfit: ', totalProfit);
+  console.log('hasCurrency: ', hasCurrency);
+
+  if (!hasCurrency) {
+   console.log('ticks: ', ticks);
+  }
 
   // TODO: improve this?
   // TODO: use ticker.date instead?
-  if (ticks > 500 && !hasCurrency) {
+  if (ticks > maximumTicks && !hasCurrency) {
     ticks = 0;
     priceToBuyAt = undefined;
   }
 
   if (priceToBuyAt === undefined) {
-    console.log('Setting buy and sell prices.')
-    priceToBuyAt = currentPrice - currentPrice * profitPercentage;
+    console.log('Setting buy and sell prices.');
     priceToSellAt = currentPrice;
+    priceToBuyAt = currentPrice - currentPrice * profitPercentage;
   }
 
   if (!hasCurrency && shouldBuyCurrency(currentPrice)) {
-    console.log("Buying currency.");
+    console.log('Buying currency.');
     hasCurrency = true;
   }
 
   if (hasCurrency && shouldSellCurrency(currentPrice)) {
-    console.log("Selling currency.");
+    console.log('Selling currency.');
     totalProfit = totalProfit + (priceToSellAt - priceToBuyAt);
     amountOfDeals++;
     reset();
