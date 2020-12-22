8f460cc1c19a89e25e9147f8031ecaaf7e2ebfa9
undeadlol1 <undeadlol1@mail.ru>
2020-12-22 05:03:48 +0500

    feat: :loud_sound: add better logging and bot pausing

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const ccxt = require('ccxt');
+const logger = require('signale');
+const interactiveLogger = new logger.Signale({
+  interactive: true,
+});
+
 let ticks = 0;
 let totalProfit = 0;
 let amountOfDeals = 0;
@@ -8,52 +14,69 @@ let priceToSellAt = undefined;
 const intervalDuration = 1000;
 const profitPercentage = 0.002;
 
-const ccxt = require('ccxt');
+let isBotPaused = false;
+
 const exchange = new ccxt.binance({ enableRateLimit: false });
 
 setInterval(runStrategy, intervalDuration);
 
 async function runStrategy() {
-  ticks++;
+  if (isBotPaused) {
+    return;
+  }
 
   const ticker = await exchange.fetchTicker('ETH/USDT');
   const currentPrice = ticker && ticker.close;
 
-  logInfoToConsole(currentPrice);
+  ticks++;
+
+  interactiveLogger.info('Current price: %d', currentPrice);
 
   if (ticks > maximumTicks && !hasCurrency) {
+    logger.warn('Resetting order prices.');
     ticks = 0;
     priceToBuyAt = undefined;
   }
 
   if (priceToBuyAt === undefined) {
-    console.log('Setting buy and sell prices.');
     priceToSellAt = currentPrice;
     priceToBuyAt = currentPrice - currentPrice * profitPercentage;
+    logger.info('Calculating prices.');
+    logger.info('Price to buy at: ', priceToBuyAt);
+    logger.info('Price to sell at: ', priceToSellAt);
   }
 
   if (!hasCurrency && shouldBuyCurrency(currentPrice)) {
-    console.log('Buying currency.');
+    logger.info('Buying currency.');
     hasCurrency = true;
   }
 
   if (hasCurrency && shouldSellCurrency(currentPrice)) {
-    console.log('Selling currency.');
+    logger.info('Selling currency.');
     totalProfit = totalProfit + (priceToSellAt - priceToBuyAt);
     amountOfDeals++;
+    logger.success('Amount of deals:', amountOfDeals);
+    logger.success('Total profit: ', totalProfit);
     reset();
+    temporarlyPauseBot();
   }
 }
 
+function temporarlyPauseBot() {
+  logger.info('Temporarly pausing bot.');
+  isBotPaused = true;
+  setTimeout(() => (isBotPaused = false), 10000);
+}
+
 function logInfoToConsole(currentPrice) {
-  console.log('currentPrice: ', currentPrice.toFixed(2));
-  console.log('priceToBuyAt: ', priceToBuyAt && priceToBuyAt.toFixed(2));
-  console.log('priceToSellAt: ', priceToBuyAt && priceToSellAt.toFixed(2));
-  console.log('amountOfDeals: ', amountOfDeals);
-  console.log('totalProfit: ', totalProfit);
-  console.log('hasCurrency: ', hasCurrency);
+  logger.log('currentPrice: ', currentPrice.toFixed(2));
+  logger.log('priceToBuyAt: ', priceToBuyAt && priceToBuyAt.toFixed(2));
+  logger.log('priceToSellAt:', priceToBuyAt && priceToSellAt.toFixed(2));
+  logger.log('amountOfDeals:', amountOfDeals);
+  logger.log('totalProfit: ', totalProfit);
+  logger.log('hasCurrency: ', hasCurrency);
   if (!hasCurrency) {
-   console.log('ticks: ', ticks);
+    logger.log('ticks: ', ticks);
   }
 }
 
