aa4973ba3fadd2de7e9ac7cc1ad10152c1cf9c77
Mihail Paley <undeadlol1@mail.ru>
2021-03-24 17:05:49 +0500

    feat(WIP): :construction:

--- src/repositories/bot.repository.ts
+++ src/repositories/bot.repository.ts
@@ -1,9 +1,10 @@
 import { Bot } from '../entities/Bot';
 import { PrismaClient } from '@prisma/client';
+
 const prisma = new PrismaClient();
 
 export class BotRepository {
-  async create(): Bot {
-    // return PrismaClient.
+  async create(): Promise<Bot> {
+    return prisma.bot.create();
   }
 }
--- src/use_cases/__spec__.ts/buy_sell_repeat.bot.create.test.ts
+++ src/use_cases/__spec__.ts/buy_sell_repeat.bot.create.test.ts
@@ -1,6 +1,7 @@
+import { Bot } from '../../../dist/entities/Bot';
 import { Strategy } from '../../../dist/entities/Strategy';
-import { StrategyCodeName } from '../../entities/StrategyCodeName';
 import { buySellRepeatBot } from '../buy_sell_repeat.bot.create';
+import { StrategyCodeName } from '../../entities/StrategyCodeName';
 
 describe('Create BUY_SELL_REPEAT Bot', () => {
   it('returns Bot entity', async () => {
@@ -8,10 +9,13 @@ describe('Create BUY_SELL_REPEAT Bot', () => {
       payload: {},
       name: StrategyCodeName.buySellRepeat,
     };
-    const createdBot = await buySellRepeatBot({
-      isActive: true,
-      strategy: botStrategy,
-    });
+    const createdBot = await buySellRepeatBot(
+      {
+        isActive: true,
+        strategy: botStrategy,
+      },
+      { botRepository: { create: () => Promise.resolve({} as Bot) } }
+    );
     expect(createdBot.strategy).toEqual(botStrategy);
   });
 });
