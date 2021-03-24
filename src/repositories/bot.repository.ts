076ef1e8dbffbc0f2fdea2a98b596f3d064fd388
Mihail Paley <undeadlol1@mail.ru>
2021-03-24 23:50:58 +0500

    feat(WIP): :construction: add test to check if repository function is called

--- src/repositories/bot.repository.ts
+++ src/repositories/bot.repository.ts
@@ -1,10 +1,11 @@
 import { Bot } from '../entities/Bot';
 import { PrismaClient } from '@prisma/client';
+import { Strategy } from '../../dist/entities/Strategy';
 
 const prisma = new PrismaClient();
 
 export class BotRepository {
-  async create(): Promise<Bot> {
-    return prisma.bot.create();
+  async create(bot: { strategy: Strategy; name: String }): Promise<Bot> {
+    return prisma.bot.create({ data: bot, });
   }
 }
--- src/use_cases/__spec__.ts/buy_sell_repeat.bot.create.test.ts
+++ src/use_cases/__spec__.ts/buy_sell_repeat.bot.create.test.ts
@@ -3,12 +3,13 @@ import { Strategy } from '../../../dist/entities/Strategy';
 import { buySellRepeatBot } from '../buy_sell_repeat.bot.create';
 import { StrategyCodeName } from '../../entities/StrategyCodeName';
 
+const botStrategy: Strategy = {
+  payload: {},
+  name: StrategyCodeName.buySellRepeat,
+};
+
 describe('Create BUY_SELL_REPEAT Bot', () => {
   it('returns Bot entity', async () => {
-    const botStrategy: Strategy = {
-      payload: {},
-      name: StrategyCodeName.buySellRepeat,
-    };
     const createdBot = await buySellRepeatBot(
       {
         isActive: true,
@@ -17,5 +18,17 @@ describe('Create BUY_SELL_REPEAT Bot', () => {
       { botRepository: { create: () => Promise.resolve({} as Bot) } }
     );
     expect(createdBot.strategy).toEqual(botStrategy);
+  });
+
+  it('calls bot repository', async () => {
+    const createFunctionMock = jest.fn(() => Promise.resolve({} as Bot));
+    await buySellRepeatBot(
+      {
+        isActive: true,
+        strategy: botStrategy,
+      },
+      { botRepository: { create: createFunctionMock } }
+    );
+    expect(createFunctionMock.mock.calls.length === 1).toBeTruthy();
   });
 });
