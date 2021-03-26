36cbc1dd3ec3ae70ffa70133a900f0e58bab1844
Mihail Paley <undeadlol1@mail.ru>
2021-03-26 14:48:11 +0500

    feat(WIP): adding bot runner function

--- src/use_cases/run_buy_sell_repeat_bot.ts
+++ src/use_cases/run_buy_sell_repeat_bot.ts
@@ -1,14 +1,20 @@
 import { CryptoCurrencyTicker } from '../entities/CryptoCurrencyTicker';
 import { BuySellRepeatBot } from '../entities/BuySellRepeatBot';
 
-export function runBuySellRepeatBot({}: // bot,
-// ticker,
-// dependencies,
-{
+export function runBuySellRepeatBot({
+  bot,
+  ticker,
+  dependencies,
+}: {
   bot: BuySellRepeatBot;
   ticker: CryptoCurrencyTicker;
   dependencies: any;
 }): Promise<void> {
+  if (!bot.isActive) {
+    return Promise.resolve();
+  }
+  ticker.close;
+  dependencies;
   // console.log('bot: ', bot);
   // console.log('ticker', ticker);
   // console.log('dependencies', dependencies);
