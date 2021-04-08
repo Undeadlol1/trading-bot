b951707b8261466da2b6b2b1409a4925b6544e41
Mihail Paley <undeadlol1@mail.ru>
2021-04-08 22:05:14 +0500

    feat(tests): add bot data generator

---
+++ src/testing_utilities/data_generators.ts
@@ -0,0 +1,21 @@
+import { BuySellRepeatBot } from '../entities/BuySellRepeatBot';
+
+export async function generateRandomBot(): Promise<BuySellRepeatBot> {
+  const aBot: BuySellRepeatBot = {
+    id: String(Math.floor(Math.random() * 100000)),
+    buyAt: 100,
+    sellAt: 110,
+    isActive: true,
+    hasSold: false,
+    amountToBuy: 2,
+    hasBought: false,
+    symbolToBuy: 'BTC',
+    currentBalance: 1000,
+    initialBalance: 1000,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    symbolToBuyFor: 'USDT',
+    isPaperTradingEnabled: false,
+  };
+  return aBot;
+}
