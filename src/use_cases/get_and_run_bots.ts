fd0b6d0d9892b35f524125d100371e7229d6517e
undeadlol1 <undeadlol1@mail.ru>
2021-04-10 00:10:46 +0500

    fix: bots used to use improper ticker

--- src/use_cases/get_and_run_bots.ts
+++ src/use_cases/get_and_run_bots.ts
@@ -1,28 +1,37 @@
-import { BuySellRepeatBotRepo } from '../repositories/buy_sell_repeat_bot.repository';
 import { BuySellRepeatBotRunner } from './buy_sell_repeat_bot.runner';
 import { OrderRepository } from '../repositories/order.create.repository';
+import { BuySellRepeatBotRepo } from '../repositories/buy_sell_repeat_bot.repository';
+import { GetMultipleCryptoTickers } from '../repositories/get_multiple_crypto_tickers';
+import { BuySellRepeatBot } from '../entities/BuySellRepeatBot';
 
 export async function getAndRunBots({
   di,
 }: {
-  di: { botRepo: BuySellRepeatBotRepo; createOrder: OrderRepository };
+  di: {
+    botRepo: BuySellRepeatBotRepo;
+    createOrder: OrderRepository;
+    getTickers: GetMultipleCryptoTickers;
+  };
 }): Promise<void> {
   const bots = await di.botRepo.findMany({ where: { isActive: true } });
-  const botRunners: BuySellRepeatBotRunner[] = bots.map(bot => {
-    const runner = new BuySellRepeatBotRunner({
-      bot,
-      // TODO fetch tickers
-      ticker: {
-        close: 10,
-        symbol: bot.symbolToBuy,
-      },
-      dependencies: {
-        updateBot: di.botRepo.update,
-        createOrder: di.createOrder.create,
-      },
-    });
-    return runner;
+  const tickers = await di.getTickers.run({ symbols: bots.map(i => i.symbol) });
+  const botRunners: (BuySellRepeatBotRunner | undefined)[] = bots.map(function(
+    bot: BuySellRepeatBot
+  ): BuySellRepeatBotRunner | undefined {
+    const ticker = tickers?.find(ticker => ticker.symbol === bot.symbol);
+    if (ticker) {
+      return new BuySellRepeatBotRunner({
+        bot,
+        ticker,
+        dependencies: {
+          updateBot: di.botRepo.update,
+          createOrder: di.createOrder.create,
+        },
+      });
+    } else {
+      return;
+    }
   });
 
-  await Promise.all(botRunners.map(runner => runner.run()));
+  await Promise.all(botRunners.map(runner => runner?.run()));
 }
