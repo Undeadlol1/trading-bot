337bd19650c3c695736df3b9f409dc1490d52824
Mihail Paley <undeadlol1@mail.ru>
2021-04-15 17:54:27 +0500

    fix: :white_check_mark: wrong entity in data generator

--- src/testing_utilities/data_generators.ts
+++ src/testing_utilities/data_generators.ts
@@ -3,18 +3,17 @@ import { BuySellRepeatBot } from '../entities/BuySellRepeatBot';
 export async function generateRandomBot(): Promise<BuySellRepeatBot> {
   const aBot: BuySellRepeatBot = {
     id: String(Math.floor(Math.random() * 100000)),
+    amount: 2,
     buyAt: 100,
     sellAt: 110,
     isActive: true,
     hasSold: false,
-    amount: 2,
     hasBought: false,
-    symbolToBuy: 'BTC',
+    symbol: 'BTCUSDT',
     currentBalance: 1000,
     initialBalance: 1000,
     createdAt: new Date(),
     updatedAt: new Date(),
-    symbolToBuyFor: 'USDT',
     isPaperTradingEnabled: false,
   };
   return aBot;
